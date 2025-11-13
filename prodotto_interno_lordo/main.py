0e09ebf1feda933cda48feae31efc7e0676a37be
Angelo200802 <angelo.capogrosso02@gmail.com>
2025-11-13 17:55:56 +0100

    Enhance data processing script: add local path fallback, integrate linear model analysis, and generate time series plot; update requirements for pandas and openpyxl.

--- prodotto_interno_lordo/main.py
+++ prodotto_interno_lordo/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os
+import os,pandas as pd
 import rpy2.robjects as r
 import rpy2.robjects.packages as rpackages
 from rpy2.robjects.vectors import StrVector
@@ -11,6 +11,8 @@ if __name__ == "__main__":
 
     if PATH is None:
         print("DATASET_PATH is not set in the environment variables.")
+        print("Using local path...")
+        DATASET_PATH = DATASET_NAME
     else: 
         DATASET_PATH = PATH+"/"+DATASET_NAME
 
@@ -25,7 +27,31 @@ if __name__ == "__main__":
     
     df = r.r(f'''
         library(readxl)
-        read_excel("{DATASET_PATH}", skip = 8)
+library(dplyr)
+
+df <- read_excel("{DATASET_PATH}", skip = 7, col_names = FALSE)
+df <- df %>% filter(!if_all(-1, is.na))
+valori <- df[2, -1] %>% unlist()
+valori <- as.numeric(gsub(",", ".", trimws(valori)))
+valori <- valori[!is.na(valori)]
+ts_pil <- ts(valori, start = c(1996, 1), frequency = 4)
+ts_pil
     ''')
 
-    print(df)
+    r.globalenv['ts_pil'] = df
+
+    r.r('''
+        png("serie_trimestrale.png", width=800, height=600)
+        plot(ts_pil, main="Serie Trimestrale del Prodotto Interno Lordo", ylab="PIL", xlab="Anno", col="blue")
+        dev.off()
+    ''')
+
+    m = r.r('''
+        t <- seq(1, length(ts_pil),1)
+        m <- lm(ts_pil ~ poly(t,3,raw=TRUE))
+        summary(m)
+    ''')
+
+    print("DATASET:\n", df)
+
+    print("Summary of Linear Model M:\n",m)
