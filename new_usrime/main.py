e521cd47e8c7e4921b4093442a81e3256291b1c2
Angelo200802 <angelo.capogrosso02@gmail.com>
2025-11-16 18:22:53 +0100

    Add histogram and scatterplot matrix generation for crime data analysis

--- new_usrime/main.py
+++ new_usrime/main.py
@@ -2,6 +2,19 @@ from dotenv import load_dotenv
 import os,pandas as pd
 import rpy2.robjects as r
 from rpy2.robjects import pandas2ri
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
+
+def install_packages(package_names):
+    """Install R packages if they are not already installed."""
+    utils = rpackages.importr('utils')
+    utils.chooseCRANmirror(ind=1)  # Select the first CRAN mirror
+    for package in package_names:
+        if not rpackages.isinstalled(package):
+            utils.install_packages(StrVector([package]))
+            print(f"📦 Pacchetto '{package}' installato con successo!")
+        else:
+            print(f"✅ Il pacchetto '{package}' è già installato in R.")
 
 if __name__ == "__main__":
     load_dotenv()
@@ -15,13 +28,15 @@ if __name__ == "__main__":
     else: 
         DATASET_PATH = PATH+"/"+DATASET_NAME
 
+    install_packages(['DAAG', 'car'])
+
     ds = r.r(f'''
     df <- read.table(file="{DATASET_PATH}", header=TRUE, sep="\t", dec=",")
     df
     ''')
     r.globalenv['ds'] = ds
 
-    ds = pandas2ri.rpy2py(ds)
+    ds : pd.DataFrame = pandas2ri.rpy2py(ds)
 
     print(f"Dataset:\n{ds}")
     for col,i in zip(ds.columns, range(1,len(ds.columns)+1)):
@@ -36,7 +51,34 @@ if __name__ == "__main__":
         summary(W)
     ''')
 
-    print(f"Matrix W:\n{W}")    
+    print(f"Matrix W:\n{W}")  
+
+    r.r(f'png("{PATH}/histograms_crime.png", width=800, height=600); hist(Crime, main="Histogram of Crime", xlab="Crime"); dev.off()')
+
+    print("Variance and CoVariance Matrix:\n",
+          r.r('''
+        var_matrix <- var(W)
+        var_matrix
+    ''')) 
+
+    print("Correlation Matrix:\n",
+          r.r('''
+        cor_matrix <- cor(W)
+        cor_matrix
+    '''))
+
+    r.r(f'''
+        library(DAAG)
+        library(car)
+        png("{PATH}/scatterplot_matrix_crime.png", width=800, height=600)
+        scatterplotMatrix(~{"+".join(ds.columns.tolist())}, col="black",
+            pch=20, regLine = list(method=lm, lty=1, lwd=2, col="chartreuse3"),
+            smooth=FALSE,
+            diagonal=list(method ="histogram", breaks="FD"),
+            main="Matrice di dispersione con rette di regressione",
+            data=ds
+        )
+        dev.off()''')
 
 
     
